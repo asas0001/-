77d6b1790d4361055cdac0dbda6896887f9edff4
asas0001 <105582532+asas0001@users.noreply.github.com>
2022-11-17 15:22:01 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -343,11 +343,11 @@ function shareText(score) {
         return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
     }
     SubmitResults();
-    if (score <= 30) return '鸡你太美';
-    if (score <= 60) return '鸡你太太美';
-    if (score <= 100) return '鸡鸡鸡鸡鸡';
-    if (score <= 150) return '唱 跳 rap 篮球';
-    return '你就是蔡徐坤本人？';
+    if (score <= 30) return '数学！';
+    if (score <= 60) return '数学真美妙';
+    if (score <= 100) return '死吧数学';
+    if (score <= 150) return '数学地狱';
+    return '你就是魏妍本人？';
 }
 
 function toStr(obj) {
